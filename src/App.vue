39649c546944f74fc8cc0eda012293207fa97e7e
Nerakolo <nerakolo@outlook.com>
2024-10-09 17:37:00 +0800

    增加页脚，修改滚动条

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
     <Navbar style="width: 100%;z-index: 99;"/>
-    <router-view style="margin-top: 72px;"></router-view>
+    <router-view style="margin-top: 72px;padding-bottom: 100px;"></router-view>
     <!-- <transition name="fade" mode="out-in">
       <router-view></router-view>
     </transition> -->
+    <Footerx style="bottom: 0;position: absolute;"></Footerx>
 </template>
 
 <script>
   import Navbar from './components/navbar.vue'
+  import Footerx from './components/footerx.vue';
   export default{
-    components:{Navbar},
+    components:{Navbar,Footerx},
     data(){
         return{
 
@@ -35,6 +37,7 @@
   font-family: 'pingfang';
   background-color: var(--background-color);
   min-height: calc(100vh - 72px);
+  position: relative;
 }
 body{
   background-color: var(--background-color);
@@ -44,5 +47,28 @@ body{
 }
 .fade-enter-from, .fade-leave-to {
   opacity: 0;
+}
+
+/* 滚动条的宽度和轨道样式 */
+body::-webkit-scrollbar {
+  width:4px;
+}
+/* 滚动条轨道 */
+body::-webkit-scrollbar-track {
+  background: var(--background-color);
+}
+
+/* 滚动条手柄 */
+body::-webkit-scrollbar-thumb {
+  background: var(--icon-theme-color-blur);
+  border-radius: 4px;
+}
+
+/* 滚动条手柄悬停效果 */
+body::-webkit-scrollbar-thumb:hover {
+  background: var(--text-color);
+}
+html{
+  scroll-behavior: smooth;
 }
 </style>
